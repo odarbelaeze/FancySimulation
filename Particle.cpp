299310aeef6d188d912c42c7e1c2db1ec07de58d
Juan Camilo Henao <juanhenao21@gmail.com>
2012-08-29 22:24:11 -0500

    I finished my task in Particle.cpp

--- Particle.cpp
+++ Particle.cpp
@@ -1,6 +1,6 @@
 #include "Particle.hpp"
 
-#include <cassert>
+#include <exception>
 
 /*
     Creates a null instance of Particle.
@@ -11,34 +11,48 @@ Particle::Particle() {
 }
 
 /*
-	Creates a Particle instance with a given pos
+    Creates a Particle instance with a given pos
 */
 
 Particle::Particle(Vec pos){
-	assert(pos.size() == 3);
-	this.pos = pos;
+    if (pos.size() != 3) throw exception();
+    this.pos = pos;
 }
 
 /* 
-	Creates a Particle instance with a type and a given position
+    Creates a Particle instance with a given id and a given position
 */
 
 Particle::Particle(string id, Vec pos){
-	assert(pos.size() == 3);
-	this.id = id;
-	this.pos = pos;
+    if (pos.size() != 3) throw exception();
+    this.id = id;
+    this.pos = pos;
 
 }
 
 /*
-	Creates a Particle instance with a given position and a given spin
+    Creates a Particle instance with a given position and a given spin
 */
 
 Particle::Particle(Vec pos, Vec s){
+    if (pos.size() != 3) throw exception();
+    if (s.size() != 3) throw exception();
+    this.pos = pos;
+    this.s = s;
+
 
 }
 
+/*
+    Creates a Particle instance with a given id,  given position and given spin
+*/
+
 Particle::Particle(string id, Vec pos, Vec s){
+    if (pos.size() != 3) throw exception();
+    if (s.size() != 3) throw exception();
+    this.id = id;
+    this.pos = pos;
+    this.s = s;
 
 }
 
@@ -52,20 +66,47 @@ double Particle::spinNorm(){
 
 }
 
+/*
+    Creates a Particle instance with a given norm and a given r
+*/
+
 Vec Particle::randSpin(double norm, double r){
+    if (norm < 0) throw exception();
+    if (r < 0) throw exception();
+    this.norm = norm;
+    this.r = r;
 
 }
+
+/*
+    Creates a Particle instance with a given norm
+*/
 
 Vec Particle::cheapRandSpin(double norm){
+    if (norm < 0) throw exception();
+    this.norm = norm;
 
 }
+
+/*
+    Creates a Particle instance with a given spin new_s
+*/
 
 
 void Particle::changeSpinTo(Vec new_s){
+    if (new_s.size() != 3) throw exception;
+    this.new_s = new_s;
 
 }
 
+/*
+    Creates a Particle instance with a given r
+*/
+
 void Particle::changeSpin(double r){
+    if (r < 0) throw exception;
+    this.r = r;
+
 
 }
 
@@ -77,17 +118,25 @@ void Particle::commitSpin(){
 
 }
 
+/*
+    Creates a Particle instance with a given position new_pos
+*/
 
 void Particle::moveTo(Vec new_pos){
+    if (new_pos.size() != 3) throw exception;
+    this.new_pos = new_pos;
 
 }
 
 void Particle::move(Vec delta_pos){
+    if (delta_pos.size() != 3) throw exception;
+    this.delta_pos = delta_pos;
 
 }
 
 
 void Particle::setId(string id){
+    this.id = id;
 
 }
 

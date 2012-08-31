0bfebf94fb24842e1eedc6d0ae84da9bf50781ea
Oscar David Arbeláez <odarbelaeze@gmail.com>
2012-08-31 16:17:44 -0500

    System.cpp drafted but everything to implement.

---
+++ System.cpp
@@ -0,0 +1,68 @@
+#include "System.hpp"
+#include "Particle.hpp"
+
+using namespace std;
+
+/*
+    TODO: Coment & implement
+*/
+
+System::System(){
+
+}
+
+/*
+    TODO: Coment & implement
+*/
+
+System::System(ifstream& file){
+
+}
+
+/*
+    TODO: Coment & implement
+*/
+
+System::~System(){
+
+}
+
+/*
+    TODO: Coment & implement
+*/
+
+double System::energy(){
+
+}
+
+/*
+    TODO: Coment & implement
+*/
+
+double System::energy(int i){
+
+}
+
+/*
+    TODO: Coment & implement
+*/
+
+Vec System::magnetization(){
+
+}
+
+/*
+    TODO: Coment & implement
+*/
+
+void System::estabilizeAt(Vec B, double T, int mcs){
+
+}
+
+/*
+    TODO: Coment & implement
+*/
+
+void System::measureAt(Vec B, double T, int mcs){
+
+}
